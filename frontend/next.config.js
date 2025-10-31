5033b6c438a12b86b200ff0b73f56902d3d7d021
kaminoguo <Lyrica2333@gmail.com>
2025-10-31 08:43:46 +0800

    fix: convert next.config.js back to CommonJS

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,4 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+const createNextIntlPlugin = require('next-intl/plugin');
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
@@ -10,4 +10,4 @@ const nextConfig = {
   },
 }
 
-export default withNextIntl(nextConfig);
+module.exports = withNextIntl(nextConfig);
